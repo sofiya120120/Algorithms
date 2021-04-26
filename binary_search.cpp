a1e16604adae695948072765b3f62761d74dd3f1
sofiya120120 <80824844+sofiya120120@users.noreply.github.com>
2021-04-26 08:50:45 +0300

    Add files via upload
    Дан массив из N целых чисел. Все числа от−109 до 109.
    Нужно уметь отвечать на запросы вида “Cколько чисел имеют значения от L до R?”.
    
    Формат ввода
    Число N (1≤N≤105). Далее N целых чисел.
    Затем число запросов K (1≤K≤105). Далее K пар чисел L,  (−109≤L≤R≤109) — собственно запросы.
    
    Формат вывода
    Выведите K чисел — ответы на запросы.

--- binary_search.cpp
+++ binary_search.cpp
@@ -3,24 +3,44 @@
 using namespace std;
 
 int main() {
-    vector<int> a = {1, 3, 5, 8, 10, 11, 14, 19, 100};
-    int x = 111, l = 0, r = 9, m;
-    bool found = false;
-    while (l < r) {
-        m = (l + r) / 2;
-        if (a[m] == x) {
-            cout << "exist";
-            found = true;
-            break;
-        } else if (a[m] > x) {
-            r = m - 1;
-        } else {
-            l = m + 1;
-        }
+    int n, k;
+    cin >> n;
+    vector<int> base(n);
+    for (int i = 0; i < n; i++) {
+        cin >> base[i];
     }
-    if (!found) {
-        if (a[l] == x) cout << "exist";
-        else cout << "no";
+    cin >> k;
+    sort(base.begin(), base.end());
+    int a, b, x1, x2;
+    for (int i = 0; i < k; i++) {
+        cin >> a >> b;
+        int left = 0, right = n - 1, mid;
+        while (left < right - 1) {
+            mid = (left + right) / 2;
+            if (base[mid] >= a) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        x1 = left;
+        while (x1 < n && base[x1] < a) {
+            x1++;
+        }
+        left = 0, right = n - 1;
+        while (left < right - 1) {
+            mid = (left + right) / 2;
+            if (base[mid] <= b) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        x2 = right;
+        while (x2 >= 0 && base[x2] > b) {
+            x2--;
+        }
+        cout << max(0, x2 - x1 + 1) << " ";
     }
     return 0;
 }
